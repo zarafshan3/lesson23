62e766bac3c48e1076b1553d7e0a3927276c4afd
zarafshan3 <ejazzarafshan@gmail.com>
2024-08-10 14:20:42 +0400

    first commit

---
+++ java/js.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
+
+
+
